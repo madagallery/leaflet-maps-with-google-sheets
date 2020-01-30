c6b81461b4bb3529468ee5a2d98238c3a08f5018
madagallery <57764813+madagallery@users.noreply.github.com>
2020-01-30 12:31:45 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kOdC0YB5O7vbLC0mSNY8hrx3oIUcbUu3zv-FneKYs0o/edit#gid=0';
